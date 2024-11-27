ce4a2cfbb8ca3a15649a48d73edb88d76e4dbd24
Manar Alsetrawi <202200412@student.polytechnic.bh>
2024-11-27 12:39:31 +0300

    Add files via upload

---
+++ Hirely/Hirely/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hirely
+//
+//  Created by Manar Alsatrawi on 27/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
